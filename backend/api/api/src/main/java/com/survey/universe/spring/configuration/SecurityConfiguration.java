bdf5e7aad24fd98ea9077be762513e508d416da8
IvanOmelchenkoIP <io25112002@gmail.com>
2026-05-24 14:22:41 +0300

    Fix CORS port

--- backend/api/api/src/main/java/com/survey/universe/spring/configuration/SecurityConfiguration.java
+++ backend/api/api/src/main/java/com/survey/universe/spring/configuration/SecurityConfiguration.java
@@ -46,7 +46,7 @@ public class SecurityConfiguration {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+		configuration.setAllowedOrigins(List.of("http://localhost:4200"));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 		configuration.setExposedHeaders(List.of("Authorization"));
