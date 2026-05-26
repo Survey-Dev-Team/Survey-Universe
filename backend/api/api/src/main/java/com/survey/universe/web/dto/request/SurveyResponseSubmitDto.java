778ce770cdc6f70d96be2fed9cd0044d30d004d6
IvanOmelchenkoIP <io25112002@gmail.com>
2026-05-26 05:50:58 +0300

    Fix login and response submission

--- backend/api/api/src/main/java/com/survey/universe/web/dto/request/SurveyResponseSubmitDto.java
+++ backend/api/api/src/main/java/com/survey/universe/web/dto/request/SurveyResponseSubmitDto.java
@@ -7,7 +7,8 @@ import com.survey.universe.web.dto.AnswerDto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
-public record SurveyResponseSubmitDto(@Valid @NotEmpty List<AnswerDto> answers, @NotBlank Boolean isComplete) {
+public record SurveyResponseSubmitDto(@Valid @NotEmpty List<AnswerDto> answers, @NotNull Boolean isComplete) {
 
 }
